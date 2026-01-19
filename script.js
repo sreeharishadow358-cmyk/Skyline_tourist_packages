c1b3dfd20b50377f7876d3d68d9e81bdd2cfd1f8
sreeharishadow358-cmyk <sreeharishadow358@gmail.com>
2026-01-19 17:09:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+document.getElementById("year").innerText = new Date().getFullYear();
